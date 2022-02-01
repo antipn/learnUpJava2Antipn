7e92e255cbd53e9f21a829e568e5885b69cb8378
antipn <palmer@bk.ru>
2022-02-02 01:28:03 +0300

    initial empty Maven project has been created

---
+++ src/main/java/ru/learnup/java2/antipn/Main.java
@@ -0,0 +1,10 @@
+package ru.learnup.java2.antipn;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, world, this is project of antipn");
+
+
+    }
+
+}
